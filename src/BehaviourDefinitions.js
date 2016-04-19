fb597d0819479c4c000205a1733c9048e4dbfd4a
John Grey <jgrey@ucsc.edu>
2016-04-19 02:47:50 -0700

    added behaviour to select a colour for each agent, and colour moves

--- src/BehaviourDefinitions.js
+++ src/BehaviourDefinitions.js
@@ -1,24 +1,49 @@
 /* jshint esversion : 6 */
 define(['underscore'],function(_){
-
+    
     let BModule = [];
 
     BModule.push(function(bTree){
         bTree.Behaviour('initialTree')
-            .type('parallel')
+            .type('sequential')
             .persistent(true)
-            .children('move');
+            .children('genColour','move');
     });
 
     BModule.push(function(bTree){
+        bTree.Behaviour('genColour')
+            .specificity(5)
+            .priority(2)
+            .entryCondition(a=>`!!.${a.values.name}.colour`)
+            .performAction(a=>a.assert(`.${a.values.name}.colour!${rndColour()}`));
+    });
+
+    BModule.push(function(bTree){
+        bTree.Behaviour('genColour');
+    });
+    
+    BModule.push(function(bTree){
         bTree.Behaviour('move')
+            .priority(1)
         //no entry condition
+            .entryCondition(d=>`.${d.values.name}.colour!%{x}`)
             .performAction((ctx,n)=>{
-                console.log('moving');
-                ctx.values.board.move(ctx.id,_.sample(_.values(ctx.values.movements)));
+                let movement = _.sample(_.values(ctx.values.movements));
+                console.log(`Moving : ${movement}`);
+                ctx.values.board.positions[ctx.values.board.offsetToIndex(ctx.values)].colour = n.bindings.x;
+                ctx.values.board.move(ctx.id,movement);
             });
     });
+
+
+    function rndColour() {
+        var r = ('0' + Math.floor(Math.random() * 256).toString(16)).substr(-2), // red
+            g = ('0' + Math.floor(Math.random() * 256).toString(16)).substr(-2), // green
+            b = ('0' + Math.floor(Math.random() * 256).toString(16)).substr(-2); // blue
+        return '#' + r + g + b;
+    }
 
     
     return BModule;
 });
+
