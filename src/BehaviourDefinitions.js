320c9bbf4762ff71f14a3ad79856fa8d1a2041ee
John Grey <jgrey@ucsc.edu>
2016-05-31 20:50:51 -0700

    added meet and greet behaviour

--- src/BehaviourDefinitions.js
+++ src/BehaviourDefinitions.js
@@ -10,9 +10,9 @@ define(['lodash'],function(_){
     //The initial tree
     BModule.push(function(bTree){
         bTree.Behaviour('initialTree')
-            .type('sequential')
+            .type('parallel')
             .persistent(true)
-            .subgoal('genColour','moveToRandomTarget');
+            .subgoal('genColour','meetAndGreet');
     });
 
     //----------------------------------------
@@ -23,7 +23,8 @@ define(['lodash'],function(_){
             .preference(5)
             .priority(2)
             .entryCondition(a=>`!!.${a.values.name}.colour?`)
-            .performAction(a=>a.fb.parse(`.${a.values.name}.colour!${rndColour()}`));
+            .performAction(a=>a.fb.parse(`.${a.values.name}.colour!${rndColour()}`))
+            .subgoal('moveToRandomTarget');
     });
 
     //fallback gencolour
@@ -35,7 +36,6 @@ define(['lodash'],function(_){
     //Simple Move behaviour, goal less
     BModule.push(function(bTree){
         bTree.Behaviour('move')
-            .priority(1)
         //entry: has a colour. bind to x
             .entryCondition(d=>`.${d.values.name}.colour![1]->x?`)
         //perform: get a movement direction, move in that direction. also colour cell
@@ -59,11 +59,26 @@ define(['lodash'],function(_){
             .subgoal('followPath');
     });
 
+    //fallback move behaviour
     BModule.push(function(bTree){
         bTree.Behaviour('followPath')
+            .performAction((a,n)=>{
+                let neighbours = a.values.board.neighbourCells(a.values).filter(d=>d.blocked === false);
+                if(neighbours.length > 0){
+                    let selected = _.sample(neighbours);
+                    a.values.board.moveTo(a.id,selected.index);
+                }
+            });
+    });
+
+    //main follow path behaviour
+    BModule.push(function(bTree){
+        bTree.Behaviour('followPath')
+            .preference(5)
         //ENTRY
-            .entryCondition((a,n)=>n.parent.bindings.x !== undefined)
-            .entryAction(a=>{
+            .entryCondition((a,n)=>n.parent.bindings.x !== undefined,
+                            (a,n)=>n.parent.values.bypass === undefined)
+            .entryAction((a,n)=>{
                 //choose a random place to go to
                 let randomIndex = Math.floor(Math.random() * a.values.board.positions.length-1);
                 a.values.pathTarget = randomIndex;
@@ -73,9 +88,15 @@ define(['lodash'],function(_){
                 });
                 //generate a path
                 a.values.path = a.values.board.pathFind(currentIndex,randomIndex);
-                //store it
-                a.values.pathIndex = 0;
-                a.fb.parse(`.${a.values.name}.pathChosen`);
+                //if the path is invalid fail early
+                if(a.values.path.length === 0){
+                    n.parent.values.bypass = true;
+                    n.informParent(a.returnTypes.fail);
+                }else{                
+                    //store it
+                    a.values.pathIndex = 0;
+                    a.fb.parse(`.${a.values.name}.pathChosen`);
+                }
             })
         //PERSISTENCE:
             .persistent(true)
@@ -109,22 +130,46 @@ define(['lodash'],function(_){
         //entry //none / choose preferred greeting
             .entryCondition()
             .entryAction()
-        //wait //until near someone not greeted last
+        //wait //if there are no agents nearby
             .waitCondition((a,n)=>{
-                let index = a.values.board.offsetToIndex(a.values),
-                    neighbours = a.values.board.neighbours(index).map(d=>a.values.board.positions[d]),
-                    neighbourAgents = neighbours.filter(d=>_.keys(d.agents).length > 0);
-                return neighbourAgents.length == 0;
+                let neighbours = a.values.board.neighbourCells(a.values),
+                    agents = _.flatten(neighbours.filter(d=>_.keys(d.agents).length > 0).map(d=>_.values(d.agents)));
+                a.values.neighbourAgents = agents;
+                return a.values.neighbourAgents.length === 0;
             })
         //persist //yes, no conditions
             .persistent(true)
             .persistCondition()
         //performance // print 'hello'
-            .performAction()
+            .performAction((a,n)=>{
+                let selectedAgent = _.sample(a.values.neighbourAgents).values;
+                a.values.greetAgent = selectedAgent;
+            })
+        //subgoal to say hello:
+            .subgoal('sayHello')
         //exit // set last person greeted
             .exitAction();
     });
-    
+
+
+    //Say Hello Behaviour
+    BModule.push(function(bTree){
+        bTree.Behaviour('sayHello')
+            .type('sequential')
+        //agent must have been selected
+            .entryCondition((a,n)=>{
+                return a.values.greetAgent !== undefined;
+            })
+        //print hello agentname for the moment:
+            .performAction((a,n)=>{
+                console.log(`${a.values.name} says "hello ${a.values.greetAgent.name}"`);                
+            })
+        //cleanup:
+            .exitAction((a,n)=>{
+                delete a.values.greetAgent;
+            });
+        
+    });
     
 
     //utility function to generate a colour. from stack overflow
