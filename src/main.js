6143c7b916bfecf5569813289fd321a4a5afa889
John Grey <jgrey@ucsc.edu>
2016-06-04 14:09:44 -0700

    genmap control

--- src/main.js
+++ src/main.js
@@ -20,10 +20,12 @@ require(['d3','lodash','EL','BTree','Hexagon','BehaviourDefinitions','util'],fun
     "use strict";
     console.log('Hexagon AI Behaviour Tree Test');
     const DEBUG = false,
+          AGENTS = true,
+          GENMAP = true,
           height = 800,
           width = 800,
-          BOARDX_SIZE = 25,
-          BOARDY_SIZE = 25,
+          BOARD_X_SIZE = 25,
+          BOARD_Y_SIZE = 25,
           movements = {
               "q" : "upLeft",
               "e" : "upRight",
@@ -39,19 +41,21 @@ require(['d3','lodash','EL','BTree','Hexagon','BehaviourDefinitions','util'],fun
         lineType = 'horizontal',
         ringRadius = 1,
         timer = null,
-    //create the canvas:
+        //create the canvas:
         canvas = util.createCanvas("Hexagon AI Test",width,height),
         //Then create the hexagon board:
-        hexBoard = new Hexagon(canvas,height,width,BOARDX_SIZE,BOARDY_SIZE),
+        hexBoard = new Hexagon(canvas,height,width,BOARD_X_SIZE,BOARD_Y_SIZE),
         //Create the base agent:
         baseBTree = new BTree(undefined,BModule),
-        agents = [],
+        agents = createAgents(baseBTree,hexBoard,movements),
         //the current turn:
         turn = 0,
         //nodes that have been clicked:
         selectedNodes = [],
         //the last found path:
-        priorPath = [];
+        priorPath = [],
+        //debug gen map:
+        generatedMap = [];
 
     
     //command object for lookup
@@ -66,6 +70,7 @@ require(['d3','lodash','EL','BTree','Hexagon','BehaviourDefinitions','util'],fun
                 timer = setInterval(()=>{
                     //todo:pass in neighbours?
                     agents.forEach(d=>d.update());
+                    agents.forEach(d=>console.log(`${d.values.name} : ${_.keys(d.allRealNodes).length}`));
                     hexBoard.draw();
                     canvas.fillText(`Turn : ${turn++}`,400,-25);
                 },500);                    
@@ -82,6 +87,39 @@ require(['d3','lodash','EL','BTree','Hexagon','BehaviourDefinitions','util'],fun
         h : ()=>lineType = 'horizontal',
         t : ()=>lineType = 'vertLeft',
         u : ()=>lineType = 'vertRight',
+        //clear all:
+        c : ()=>hexBoard.positions.forEach(d=>{
+            hexBoard.block(d.index,true);
+            hexBoard.block(d.index);
+        }),
+        //gen map:
+        '.' : ()=>{
+            if(generatedMap.current === null){ return; }
+            let pathsPoints = generatedMap.edges[generatedMap.current].map(d=>[generatedMap.current,Number(d)]),
+                paths = pathsPoints.map(p=>hexBoard.pathFind(p[0],p[1])),
+                rings = _.values(generatedMap.edges)[0].map(d=>{
+                    return hexBoard.getRing(Number(d),2);
+                });
+            //hexBoard.positions.forEach(d=>hexBoard.block(d.index,true));
+            //hexBoard.positions.forEach(d=>hexBoard.block(d.index));
+            //set each path
+            paths.forEach(p=>{
+                p.forEach(d=>hexBoard.positions[d].colour = "green");
+            });
+            rings.forEach(d=>d.forEach(e=>{
+                hexBoard.positions[e].colour = "yellow";
+            }));
+
+            hexBoard.positions.forEach(d=>{
+                if(d.colour === 'black'){
+                    hexBoard.block(d.index);
+                }
+            });
+            
+        },
+        'm' : ()=>{
+            generatedMap = hexBoard.genMap3();
+        },
         //set the size of a ring/area
         number : n=>ringRadius = n,
         //do something when the user clicks
@@ -98,49 +136,6 @@ require(['d3','lodash','EL','BTree','Hexagon','BehaviourDefinitions','util'],fun
             }
         },
     };
-
-     //Creation of agents:
-    agents.push(baseBTree.newCharacter({
-        name : "bob",
-        colour : "grey",
-        q : 0,
-        r : 0,
-        board : hexBoard,
-        movements : movements
-    }));
-    agents.push(baseBTree.newCharacter({
-        name : "bill",
-        colour : "blue",
-        q : 1,
-        r : 1,
-        board : hexBoard,
-        movements : movements
-    }));
-    agents.push(baseBTree.newCharacter({
-        name : "jill",
-        colour : "green",
-        q : 3,
-        r : 5,
-        board : hexBoard,
-        movements : movements
-    }));
-    agents.push(baseBTree.newCharacter({
-        name : "jim",
-        colour : "yellow",
-        q : 8,
-        r : 2,
-        board : hexBoard,
-        movements : movements
-    }));;
-
-    //set debug flags for bob:
-    if(DEBUG){
-        agents[0].setDebugFlags('binding');
-        agents[0].setDebugFlags('actions','update','cleanup','preConflictSet','postConflictSet','failure','facts');
-    }
-    
-    //Register the agents into the board:
-    hexBoard.register(agents);
     
     //Draw the board initially:
     hexBoard.draw();
@@ -159,6 +154,7 @@ require(['d3','lodash','EL','BTree','Hexagon','BehaviourDefinitions','util'],fun
             if(d3.event.key === 'Shift'){
                 shift = false;
             }
+            hexBoard.draw();
         });
 
     //Register mouse clicks
@@ -173,17 +169,17 @@ require(['d3','lodash','EL','BTree','Hexagon','BehaviourDefinitions','util'],fun
     
     /**
        Pathfind. using the last two positions specified, run A* on them
-     */    
-    let pathFind = function(index){
-            //convert the mouse click to a position in the canvas
+    */    
+    function pathFind(index,target){
+        //convert the mouse click to a position in the canvas
         //let pos = util.screenToElementPosition(event,element),
-            //convert that to a board position
-          //  index = hexBoard.screenToIndex(pos.x,pos.y),
+        //convert that to a board position
+        //  index = hexBoard.screenToIndex(pos.x,pos.y),
         let colour = shift ? "blue" : "green";
         if(index === undefined){ return; }
         //store the position
         selectedNodes.unshift(index);
-            
+        
         hexBoard.positions[index].colour = colour;
         //uncolour old positions
         if(selectedNodes.length > 2){
@@ -206,7 +202,7 @@ require(['d3','lodash','EL','BTree','Hexagon','BehaviourDefinitions','util'],fun
     };
 
     //Find a draw a line of cells 
-    let drawLine = function(index){
+    function drawLine(index){
         //let pos = util.screenToElementPosition(event,element),
         //    index = hexBoard.screenToIndex(pos.x,pos.y),
         let theLine = hexBoard.getLine(index,lineType);
@@ -216,14 +212,62 @@ require(['d3','lodash','EL','BTree','Hexagon','BehaviourDefinitions','util'],fun
     };
 
     //Get an area of the board
-    let drawRing = function(index){
+    function drawRing(index){
         //let pos = util.screenToElementPosition(event,element),
         //    index = hexBoard.screenToIndex(pos.x,pos.y),
         let theRing = hexBoard.getRing(index,ringRadius);
         hexBoard.positions[index].colour = "grey";
         theRing.forEach(d=>hexBoard.block(d,shift));
     };
-    
     //-----
+
+    function createAgents(baseBTree,board,movements){
+        if(!AGENTS) { return []; }
+        console.log('Creating Agents');
+        let agents = [];
+        agents.push(baseBTree.newCharacter({
+            name : "bob",
+            colour : "black",
+            q : 0,
+            r : 0,
+            board : board,
+            movements : movements
+        }));
+        agents.push(baseBTree.newCharacter({
+            name : "bill",
+            colour : "black",
+            q : 1,
+            r : 1,
+            board : board,
+            movements : movements
+        }));
+        agents.push(baseBTree.newCharacter({
+            name : "jill",
+            colour : "black",
+            q : 3,
+            r : 5,
+            board : board,
+            movements : movements
+        }));
+        agents.push(baseBTree.newCharacter({
+            name : "jim",
+            colour : "black",
+            q : 8,
+            r : 2,
+            board : board,
+            movements : movements
+        }));
+
+        //set debug flags for bob:
+        if(DEBUG){
+            agents[0].setDebugFlags('binding');
+            agents[0].setDebugFlags('actions','update','cleanup','preConflictSet','postConflictSet','failure','facts');
+        }
+                
+        //Register the agents into the board:
+        hexBoard.register(agents);
+        return agents;
+    };
+        
     console.log(hexBoard);
 });
